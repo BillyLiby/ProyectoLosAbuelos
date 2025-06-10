c74c76bdc4c953d9bb29690cf6ac9c2b619e5b05
BillyLiby <velasquezbill00@gmail.com>
2025-06-10 15:55:07 -0600

    Se creo el modulo de compra, se migraron los modelos y frontend de compra

--- app/cmp/forms.py
+++ app/cmp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Proveedor
+from .models import Proveedor, ComprasEnc
 
 class ProveedorForm(forms.ModelForm):
     email = forms.EmailField(max_length=254)
@@ -16,18 +16,24 @@ class ProveedorForm(forms.ModelForm):
                 'class': 'form-control'
             })
 
-    def clean(self):
-        try:
-            sc = Proveedor.objects.get(
-                descripcion=self.cleaned_data["descripcion"].upper()
-            )
+class ComprasEncForm(forms.ModelForm):
+    fecha_compra = forms.DateInput()
+    fecha_factura = forms.DateInput()
 
-            if not self.instance.pk:
-                print("Registro ya existe")
-                raise forms.ValidationError("Registro Ya Existe")
-            elif self.instance.pk!=sc.pk:
-                print("Cambio no permitido")
-                raise forms.ValidationError("Cambio No Permitido")
-        except Proveedor.DoesNotExist:
-            pass
-        return self.cleaned_data
+    class Meta:
+        model=ComprasEnc
+        fields=['proveedor','fecha_compra','observacion',
+            'no_factura','fecha_factura','sub_total',
+            'descuento','total']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['fecha_compra'].widget.attrs['readonly'] = True
+        self.fields['fecha_factura'].widget.attrs['readonly'] = True
+        self.fields['sub_total'].widget.attrs['readonly'] = True
+        self.fields['descuento'].widget.attrs['readonly'] = True
+        self.fields['total'].widget.attrs['readonly'] = True
