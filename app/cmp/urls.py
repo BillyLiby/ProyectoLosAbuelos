3ec1c18531cd7e3019b44a725bab322dd98e8625
BillyLiby <velasquezbill00@gmail.com>
2025-06-12 14:52:08 -0600

    Ruta para reporte de compras especificas

--- app/cmp/urls.py
+++ app/cmp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from .views import ProveedorView,ProveedorNew, ProveedorEdit, proveedorInactivar,ComprasView, compras,CompraDetDelete
 
-from .reportes import reporte_compras
+from .reportes import reporte_compras, imprimir_compra
 
 urlpatterns = [
     path('proveedores/', ProveedorView.as_view(), name="proveedor_list"),
@@ -16,4 +16,6 @@ urlpatterns = [
     path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
 
     path('compras/listado', reporte_compras, name='compras_print_all'),
+    path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
+    
 ]
