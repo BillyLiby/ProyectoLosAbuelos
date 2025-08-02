6c77650b5e20af91dc4751dea555f721e0f6faf6
BillyLiby <velasquezbill00@gmail.com>
2025-08-01 18:36:22 -0600

    Validación de únicamente números en No de proveedor y No recibo en Compras

--- app/cmp/forms.py
+++ app/cmp/forms.py
@@ -15,6 +15,20 @@ class ProveedorForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class': 'form-control'
             })
+
+    # Personalizar campo teléfono
+        self.fields['telefono'].widget.attrs.update({
+            'pattern': '[0-9]+',
+            'inputmode': 'numeric',
+            'maxlength': '10',
+            'title': 'Ingrese solo números'
+        })
+
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+        if telefono and not telefono.isdigit():
+            raise forms.ValidationError("El teléfono debe contener solo números.")
+        return telefono
     
     def clean(self):
         try:
@@ -49,8 +63,24 @@ class ComprasEncForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class': 'form-control'
             })
+
+         # Atributos adicionales para no_factura
+        self.fields['no_factura'].widget.attrs.update({
+            'pattern': '[0-9]+',
+            'inputmode': 'numeric',
+            'maxlength': '20',
+            'title': 'Ingrese solo números',
+            'placeholder': 'Ej: 784456'
+        })
+
         self.fields['fecha_compra'].widget.attrs['readonly'] = True
         self.fields['fecha_factura'].widget.attrs['readonly'] = True
         self.fields['sub_total'].widget.attrs['readonly'] = True
         self.fields['descuento'].widget.attrs['readonly'] = True
         self.fields['total'].widget.attrs['readonly'] = True
+
+    def clean_no_factura(self):
+        no_factura = self.cleaned_data.get('no_factura')
+        if no_factura and not no_factura.isdigit():
+            raise forms.ValidationError("El número de recibo debe contener solo números.")
+        return no_factura
